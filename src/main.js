f6d295aed1fae8e1e4378906bc29d2f3e982f65f
Vixcity <2091283625@qq.com>
2023-09-15 18:04:24 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import moment from "moment";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
+Vue.prototype.$moment = moment;
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="home">
+    <div class="container">
+      <div class="block">
+        <span class="demonstration">年</span>
+        <el-date-picker v-model="value3" type="year" placeholder="选择年">
+        </el-date-picker>
+      </div>
+    </div>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
